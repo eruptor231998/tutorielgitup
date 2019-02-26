d48e3551dabdebb3678196ba871774bf95943bb5
Lionel Viguier <lvigui03@pcalb-mm0000.univ-jfc.fr>
2019-02-26 11:33:39 +0100

    ajout: sortie de l'app a la fermeture

--- Documents/Git/src/Application.java
+++ Documents/Git/src/Application.java
@@ -1,8 +1,16 @@
 
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+
+public class Application extends Frame implements WindowListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		new Application();
@@ -20,6 +28,54 @@ public class Application extends Frame {
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		
+		System.out.println("n");
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("n");
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("n");
+		System.exit(0);
+		
 	}
 
 }
